dd933e925063837327b473b5fe1736429edd0764
Benjamin Code <bjamescode@gmail.com>
2020-05-31 16:01:23 -0700

    Updated js to change each div when mouse moves over it. Currently only changes to the color blue. Updated site styling

--- script.js
+++ script.js
@@ -1,22 +1,37 @@
 const main = document.querySelector('main');
 
-
 function makeGrid (row) {
 main.style.gridTemplateColumns = `repeat(${row}, 1fr)`;
 
     for (let i = 0; i < row; i++) {
         const rowDiv = document.createElement('div');
-            rowDiv.style.background = 'grey';
+            rowDiv.style.background = 'white';
             rowDiv.style.width = '100%';
             rowDiv.style.height = '100%';
+            rowDiv.addEventListener("mouseover", mouseOver, false);
+            rowDiv.addEventListener("mouseout", mouseOut, false);
         main.appendChild(rowDiv);
+            function mouseOver() {
+                rowDiv.style.background = "blue";
+            }
+            function mouseOut () {
+                rowDiv.style.background = "blue";
+            }
     
     for (let j = 0; j < row - 1; j++) {
         const colDiv = document.createElement('div');
-            colDiv.style.background = 'black';
+            colDiv.style.background = 'white';
             colDiv.style.width = '100%';
             colDiv.style.height = '100%';
+            colDiv.addEventListener("mouseover", mouseOver, false);
+            colDiv.addEventListener("mouseout", mouseOut, false);
         main.appendChild(colDiv);
+            function mouseOver() {
+                colDiv.style.background = "blue";
+            }
+            function mouseOut () {
+                colDiv.style.background = "blue";
+            }
     }
 
     }

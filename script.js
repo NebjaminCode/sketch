81c474a7dee44e33aed63736209aaafb7e576ff0
Benjamin Code <bjamescode@gmail.com>
2020-06-07 18:08:24 -0700

    fixed the color changing button and the user input functions. Both fixes aren't the best, but they do work. May come back to tight things up. Also added comments to js file to explain the code. Oh and took out the redundant for loop in the random function, which was left over from when I was going to add a gradient color option

--- script.js
+++ script.js
@@ -1,18 +1,20 @@
+// establishing connection where grid will go
 const main = document.querySelector('#main');
 
+//random rgb color generator
 function random() {
-    for (let i = 0; i < 255; i++) {
             let red = Math.floor((Math.random()*255) + 1);
             let green = Math.floor((Math.random()*255) + 1);
             let blue = Math.floor((Math.random()*255) + 1);     
         let color = `rgb(${red}, ${green}, ${blue})`
         return color;
-    }
 }
 
+//makes grid with JS/DOM manipulation
 function makeGrid (row) {
 main.style.gridTemplateColumns = `repeat(${row}, 1fr)`;
-
+    
+    // first for loop creates the left most column
     for (let i = 0; i < row; i++) {
 const rowDiv = document.createElement('div');
 
@@ -28,6 +30,8 @@ const rowDiv = document.createElement('div');
                     rowDiv.style.background = 'black';
                  }
             }
+        
+        // second for loop creates the rest of the grid based on the first column count
         for (let j = 0; j < row - 1; j++) {
             const colDiv = document.createElement('div');
                 colDiv.style.background = 'white';
@@ -46,29 +50,45 @@ const rowDiv = document.createElement('div');
   }
 }
 
+// button for letting user pick their own row count
 const but = document.querySelector('#reset');
+// button for letting user choose black or color sketch mode
 const colorMode = document.querySelector('#color_mode')
 
+// what the grid autopopulates as when loaded
 makeGrid(5)
+
+// adding functionality to the refresh button. Ran into another problem here; the if/else if statements only run once, so if the user enters 0, a negative number, or NaN more than one time, the grid just doesn't load. I couldn't quite figure out a solid solve, so I just made an alert that says you get 3 and then sets the size automatically. 
 but.addEventListener('click', function newGrid() {
     clearGrid();
     gridDimension = prompt('How many rows would you like?');
-    if (gridDimension == 0) {
-        gridDimension = prompt('Please enter a number greater than 0');
-    } else if (gridDimension > 0) {
-        makeGrid(gridDimension);
-    }
-
+        if (gridDimension > 0) {
+            makeGrid(gridDimension);
+        } else if (gridDimension != Number || gridDimension == 0 || gridDimension < 0) {
+            alert(`You didn't enter an acceptable number. You get 3.`);
+            makeGrid(3)
+        }
 });
 
+// clears previous grid by removing main's children so the new grid with the user's choice can be created without error
 function clearGrid() {
     while (main.firstChild) {
         main.removeChild(main.firstChild)
     }
 }
 
-let colorChange = false;
+// really sloppy way to fix a bug. colorMode fires twice when the button is clicked and I couldn't quite find out why, so I just made an array with true/false repeating in twos. It has a limit to the number of times it'll work, but let's be honest; if you're using this site that much you're probably just wasting time.
+const colorChangeArr = [false, true, true, false, false, true, true, false, false, true, true, false, false, true, true, false, false, true, true, false, false, true, true,];
+let counter = 0;
+
+//establishing connection between the color changing controls
+let colorChange = colorChangeArr[counter];
 console.log(colorChange)
-colorMode.addEventListener('click', function(){
-    colorChange = true;  
+console.log(counter)
+
+// increases the counter, which changes colorMode's true/false status. This is where the error is occuring, I believe. 
+colorMode.addEventListener('click', function() {
+    counter += 1;
+    return colorChange = colorChangeArr[counter]
 });
+
